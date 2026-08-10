3422d369a82561062a190082da91032db6a8ce6b
tejaswini <tejaswinikadu85@gmail.com>
2026-08-11 01:48:26 +0530

    First Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,81 +1,104 @@
-import React from 'react';
+import React, {Fragment, useRef} from 'react';
 import {useGSAP} from "@gsap/react";
-import gsap from "gsap";
 import {SplitText} from "gsap/all";
+import gsap from "gsap";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
 
-    useGSAP(()=>{
-        const heroSplit = new SplitText('.title', {
-            type: 'chars, words'
-        })
+    const isMobile = useMediaQuery({maxWidth:767});
 
-        const paragraphSplit = new SplitText('.subtitle',{
-            type:'lines'
-        })
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', {type: 'chars, lines'});
 
-        heroSplit.chars.forEach((char)=> char.classList.add('text-gradient'))
+        heroSplit.chars.forEach(
+            (char) => char.classList.add('.text-gradient')
+        );
 
-        gsap.from(heroSplit.chars,{
-            yPercent: 100,
-            duration: 1.8,
-            ease: 'expo.out',
-            stagger: 0.05
-        });
+        gsap.from(heroSplit.chars, {yPercent: 100, duration: 1.8, ease: "expo.out", stagger: 0.05});
 
-        gsap.from(paragraphSplit.lines,{
+        gsap.from(paragraphSplit.lines, {
             opacity: 0,
             yPercent: 100,
             duration: 1.8,
-            ease: 'expo.out',
+            ease: "expo.out",
             stagger: 0.06,
             delay: 1,
-        })
+        });
 
         gsap.timeline({
-            scrollTrigger:{
+            scrollTrigger: {
                 trigger: '#hero',
                 start: 'top top',
-                end: 'bottom top',
+                end: "bottom top",
                 scrub: true,
+
             }
         })
-            .to('.right-leaf', {y:200},0)
-            .to('.left-leaf',{y: 200}, 0)
-    },[])
+            .to('.right-leaf',{y:200},0)
+            .to('.left-leaf',{y:-200},0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+    }, []);
+
+
     return (
+
         <>
-            <section id="hero" className="noisy">
+            <section id='hero' className="noisy">
                 <h1 className="title">MOJITO</h1>
 
-                <img src="/images/hero-left-leaf.png"
-                     alt="left-leaf"
-                className="left-leaf"/>
+                <img src='public/images/hero-left-leaf.png' alt='left-leaf' className='left-leaf'/>
+                <img src='public/images/hero-right-leaf.png' alt='right-leaf' className='right-leaf'/>
 
-                <img src="/images/hero-right-leaf.png"
-                     alt="right-leaf"
-                className="right-leaf"/>
-
-                <div className="body">
+                <div className='body'>
                     <div className='content'>
-                        <div className="space-y-5 hidden md:block">
-                            <p>Cool. Crisp. Classic </p>
-                            <p className="subtitle">\
-                            Sip the Spirit <br/>of Summer</p>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className='subtitle'>
+                                Skip the Spirit <br/> of Summer
+                            </p>
                         </div>
 
                         <div className="view-cocktails">
-                            <p className="subtitle">
-                                Every cocktail on our menu is an blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                            <p className='subtitle'>
+                                Every cocktail on our menu is a blend of premium ingredients,creative,flair,and
+                                timeless recipes - designed to delight your sense.
+
                             </p>
                             <a href="#cocktails">View Cocktails</a>
+
                         </div>
 
                     </div>
                 </div>
             </section>
+            <div className="video absolute inset-0">
+                <video ref={videoRef} src='public/videos/output.mp4' muted playsInline preload='auto'/>
+            </div>
         </>
-    );
+
+    )
+        ;
 };
 
 export default Hero;
