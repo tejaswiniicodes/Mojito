3706f37ea3269d761d0d9c49e9021c609400f8af
tejaswini <tejaswinikadu85@gmail.com>
2026-08-11 16:50:51 +0530

    First Commit

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,65 +1,73 @@
-import {cocktailLists, mockTailLists} from "../../constants/index.js";
-import {Fragment} from "react";
-import {useGSAP} from "@gsap/react";
-import gsap from 'gsap'
+import { cocktailLists, mockTailLists } from "../../constants/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Cocktails = () => {
-
-    useGSAP(()=>{
+    useGSAP(() => {
         const parallaxTimeline = gsap.timeline({
-            scrollTrigger:{
-                trigger: '#cocktails',
-                start: 'top 30%',
-                end: 'bottom 80%',
-                scrub: true,
-            }
-        })
+            scrollTrigger: {
+                trigger: "#cocktails",
+                start: "top 30%",
+                end: "bottom 80%",
+                scrubber: true,
+            },
+        });
 
-        parallaxTimeline.from('#c-left-leaf',{
-            x: 100,
-            y: 100,
-        })
-            .from('#c-right-leaf',{
-            x: 100,
-            y: 100,
-        })
-    })
+        parallaxTimeline
+            .from("#c-left-leaf", {
+                x: -100,
+                y: 100,
+            })
+            .from("#c-right-leaf", { x: 100, y: 100 });
+    });
 
     return (
-        <section id='cocktails' className="noisy">
-            <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
-            <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf"/>
+        <section id="cocktails" className="noisy">
+            <img
+                src="/public/images/cocktail-left-leaf.png"
+                alt="l-leaf"
+                id="c-left-leaf"
+            />
+            <img
+                src="/public/images/cocktail-right-leaf.png"
+                alt="r-leaf"
+                id="c-right-leaf"
+            />
 
             <div className="list">
                 <div className="popular">
-                    <h2>Most Popular cocktails</h2>
+                    <h2>Most Popular Cocktails</h2>
 
                     <ul>
-                        {cocktailLists.map(({name,country, detail, price})=>{
+                        {cocktailLists.map(({ name, country, detail, price }) => (
                             <li key={name}>
-                                <div className="md.me-28">
+                                <div className="md:me-28">
                                     <h3>{name}</h3>
-                                    <p>{country} | {detail}</p>
+                                    <p>
+                                        {country} | {detail}
+                                    </p>
                                 </div>
-                                <span>- {price}</span>
+                                <span>{price}</span>
                             </li>
-                        })}
+                        ))}
                     </ul>
                 </div>
 
                 <div className="loved">
-                    <h2>Most loved mocktails</h2>
+                    <h2>Most Loved Mocktails</h2>
 
                     <ul>
-                        {mockTailLists.map(({name,country, detail, price})=>{
+                        {mockTailLists.map(({ name, country, detail, price }) => (
                             <li key={name}>
-                                <div className="md.me-28">
+                                <div className="md:me-28">
                                     <h3>{name}</h3>
-                                    <p>{country} | {detail}</p>
+                                    <p>
+                                        {country} | {detail}
+                                    </p>
                                 </div>
-                                <span>- {price}</span>
+                                <span>{price}</span>
                             </li>
-                        })}
+                        ))}
                     </ul>
                 </div>
             </div>
