222a5142e40ba18f5606dd05b55497011d52ccc2
tejaswini <tejaswinikadu85@gmail.com>
2026-08-11 23:51:22 +0530

    First Commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,50 @@
 import { allCocktails } from "../../constants/index.js";
 import { useRef, useState } from "react";
-
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 const Menu = () => {
     const contentRef = useRef(null);
 
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    useGSAP(()=>{
+        gsap.fromTo('#title',{
+            opacity:0
+        },
+            {
+                opacity:1,
+                duration:1,
+            })
+
+        gsap.fromTo('.cocktail img',{
+            opacity: 0,
+            xPercent: -100,
+        },{
+            xPercent: 0,
+            opacity: 1,
+            duration: 1,
+            ease: 'power1.inOut'
+        })
+
+        gsap.fromTo('.details h2',{
+            opacity: 0,
+            yPercent: 100,
+        },{
+            yPercent: 0,
+            opacity: 100,
+            ease: 'power1.inOut'
+        })
+
+        gsap.fromTo('.details p',{
+            opacity: 0,
+            yPercent: 100,
+        },{
+            yPercent: 0,
+            opacity: 100,
+            ease: 'power1.inOut'
+        })
+
+    },[currentIndex])
 
     const totalCocktails = allCocktails.length;
 
